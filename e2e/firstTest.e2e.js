6c6f6b3fede0c3433517a5b72a3cba78b8f36878
Krystian Kościelniak <krystiankoscielniak@gmail.com>
2021-08-05 15:20:06 +0200

    Add detox and first working test

---
+++ e2e/firstTest.e2e.js
@@ -0,0 +1,24 @@
+/* global element device by */
+describe('React Native Paper Login Template app', () => {
+  beforeAll(async () => {
+    await device.launchApp();
+  });
+
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('should show login screen', async () => {
+    await expect(element(by.text('Login Template'))).toBeVisible();
+  });
+
+  // it('should show hello screen after tap', async () => {
+  //   await element(by.id('hello_button')).tap();
+  //   await expect(element(by.text('Hello!!!'))).toBeVisible();
+  // });
+
+  // it('should show world screen after tap', async () => {
+  //   await element(by.id('world_button')).tap();
+  //   await expect(element(by.text('World!!!'))).toBeVisible();
+  // });
+});
