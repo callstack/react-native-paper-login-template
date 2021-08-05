e3829d43406f9d41ad06c30d63835cedcff690bb
Krystian Kościelniak <krystiankoscielniak@gmail.com>
2021-08-05 17:08:44 +0200

    Add E2E tests for navigation between screens

--- e2e/app.e2e.js
+++ e2e/app.e2e.js
@@ -8,7 +8,17 @@ describe('React Native Paper Login Template app', () => {
     await device.reloadReactNative();
   });
 
-  it('should show login screen', async () => {
+  test('Application Launch', async () => {
     await expect(element(by.text('Login Template'))).toBeVisible();
+  });
+
+  test('Navigation between screens', async () => {
+    await element(by.id('login-button')).tap();
+    await element(by.id('forgot-password-link')).tap();
+    await element(by.id('login-link')).tap();
+    await element(by.id('sign-up-link')).tap();
+    await element(by.id('login-link')).tap();
+
+    await expect(element(by.id('login-screen-login-button'))).toBeVisible();
   });
 });
