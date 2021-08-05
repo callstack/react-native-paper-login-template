53ef57884a943e88c6b08094e963dbec029785d3
Krystian Kościelniak <krystiankoscielniak@gmail.com>
2021-08-05 14:11:59 +0200

    Add integration tests for RegisterScreen

---
+++ src/screens/__tests__/RegisterScreen.test.tsx
@@ -0,0 +1,116 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react-native';
+import RegisterScreen from '../RegisterScreen';
+
+describe('RegisterScreen', () => {
+  it('fires navigate() if name, email and password are valid', () => {
+    const navigation = {
+      navigate: jest.fn(),
+    };
+
+    const { getByLabelText, getByText } = render(
+      <RegisterScreen {...{ navigation }} />
+    );
+
+    const nameInput = getByLabelText('Name');
+    fireEvent.changeText(nameInput, 'Developer');
+
+    const emailInput = getByLabelText('Email');
+    fireEvent.changeText(emailInput, 'email@callstack.com');
+
+    const passwordInput = getByLabelText('Password');
+    fireEvent.changeText(passwordInput, 'somepassword');
+
+    const signUpButton = getByText(/sign up/i);
+    fireEvent.press(signUpButton);
+
+    expect(navigation.navigate).toHaveBeenCalledTimes(1);
+  });
+
+  it('does not fire navigate() if name is missing', () => {
+    const navigation = {
+      navigate: jest.fn(),
+    };
+
+    const { getByLabelText, getByText } = render(
+      <RegisterScreen {...{ navigation }} />
+    );
+
+    const emailInput = getByLabelText('Email');
+    fireEvent.changeText(emailInput, 'email@callstack.com');
+
+    const passwordInput = getByLabelText('Password');
+    fireEvent.changeText(passwordInput, 'somepassword');
+
+    const signUpButton = getByText(/sign up/i);
+    fireEvent.press(signUpButton);
+
+    expect(navigation.navigate).toHaveBeenCalledTimes(0);
+  });
+
+  it('does not fire navigate() if password is missing', () => {
+    const navigation = {
+      navigate: jest.fn(),
+    };
+
+    const { getByLabelText, getByText } = render(
+      <RegisterScreen {...{ navigation }} />
+    );
+
+    const nameInput = getByLabelText('Name');
+    fireEvent.changeText(nameInput, 'Developer');
+
+    const emailInput = getByLabelText('Email');
+    fireEvent.changeText(emailInput, 'email@callstack.com');
+
+    const signUpButton = getByText(/sign up/i);
+    fireEvent.press(signUpButton);
+
+    expect(navigation.navigate).toHaveBeenCalledTimes(0);
+  });
+
+  it('does not fire navigate() if email is missing', () => {
+    const navigation = {
+      navigate: jest.fn(),
+    };
+
+    const { getByLabelText, getByText } = render(
+      <RegisterScreen {...{ navigation }} />
+    );
+
+    const nameInput = getByLabelText('Name');
+    fireEvent.changeText(nameInput, 'Developer');
+
+    const passwordInput = getByLabelText('Password');
+    fireEvent.changeText(passwordInput, 'somepassword');
+
+    const signUpButton = getByText(/sign up/i);
+    fireEvent.press(signUpButton);
+
+    expect(navigation.navigate).toHaveBeenCalledTimes(0);
+  });
+
+  it('does not fire navigate() if email is wrong', () => {
+    const navigation = {
+      navigate: jest.fn(),
+    };
+
+    const { getByLabelText, getByText } = render(
+      <RegisterScreen {...{ navigation }} />
+    );
+
+    const nameInput = getByLabelText('Name');
+    fireEvent.changeText(nameInput, 'Developer');
+
+    const emailInput = getByLabelText('Email');
+    fireEvent.changeText(emailInput, 'wrongEmail#gmail.com');
+
+    const passwordInput = getByLabelText('Password');
+    fireEvent.changeText(passwordInput, 'somepassword');
+
+    const signUpButton = getByText(/sign up/i);
+    fireEvent.press(signUpButton);
+
+    expect(navigation.navigate).toHaveBeenCalledTimes(0);
+  });
+});
